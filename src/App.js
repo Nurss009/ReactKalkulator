de7bd1783fe9a07418b915f109b94c16e3c7f077
Nursultan <tupoukidala77@gmail.com>
2023-11-20 20:15:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './Components/style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [input1, MyInput1] = useState();
+    const [input2, MyInput2] = useState();
+    const [result, setResult] = useState();
 
-export default App;
+    const handleInput1Change = (e) => {
+        MyInput1(Number(e.target.value))
+    };
+    const handleInput2Change = (e) => {
+        MyInput2(Number(e.target.value))
+    };
+    const handleIncrease = () => {
+        setResult(input1 + input2)
+    };
+    const handleDecrease = () => {
+        setResult(input1 - input2)
+    };
+    const handleVelenie = () => {
+        if (input2 !== 0) {
+            setResult(input1 / input2);
+        }else{
+            alert("На ноль не делится")
+        }
+        setResult(input1 / input2)
+    };
+
+    const handleUmnojit = () => {
+        setResult(input1 * input2)
+    };
+
+    // const handleReset = () => {
+    //     setInput1('');
+    //     setInput2('');
+    //     setResult('');
+    // };
+
+
+    return (
+        <div className={'text'}>
+            <h1>Калькулятор</h1>
+            <input placeholder={'Введите значеня'} type="text" value={input1} onChange={handleInput1Change}/>
+            <input placeholder={'Введите значеня'} type="text" value={input2} onChange={handleInput2Change}/>
+            <div className="ca">
+                <button className={'But'} onClick={handleIncrease}>+</button>
+                <button className={'But'} onClick={handleDecrease}>-</button>
+            </div>
+            <button className={'But'} onClick={handleUmnojit}>умножение</button>
+            <button className={'But'} onClick={handleVelenie}>Деление</button>
+            {/*<button className={'But'} onClick={handleReset}> Сброс </button>*/}
+            <h2>Ответ: {result}</h2>
+        </div>
+
+    );
+};
+
+export default App
---
+++ src/Components/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css'
+
+const Button = ({text}) => {
+    const handleClick = () => {
+        console.log(`hello ${text}`)
+    }
+
+    return (
+        <button
+            className={'customButton'}
+            onClick={handleClick}
+        >
+            {text}
+        </button>
+    );
+};
+
+export default Button
+
+
+export const Input = () => {
+
+    const handleChange = (e) => {
+        console.log(e.target.value)
+    }
+    return(
+        <input
+            onChange={handleChange}
+            placeholder={'Its my input'}
+        />
+    );
+}
